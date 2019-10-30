a04100c58a202a19a9f381b7a8197d0434776234
carlosmartin667 <carloscornejomoscoso@gmail.com>
2019-10-30 11:15:29 -0300

    ejercicio 5 resuelto

--- ejercicio3/Program.vb
+++ ejercicio3/Program.vb
@@ -9,12 +9,20 @@
 
 Imports System
 
+
 Module Program
     Sub Main(args As String())
         Dim cadena, subcad, palabra, letra As String
         Dim contador = 0
         Dim largo = 0
-        cadena = "El Índice TIOBE es un índice "
+        cadena = "El Índice TIOBE es un índice elaborado por una empresa de software holandesa que se especializa en la evaluación y seguimiento de la calidad de los programas informáticos. Actualmente, la empresa revisa en tiempo real más de 300 millones de códigos de diversos programas informáticos por día actualizando mes a mes su índice."
+
+        Console.WriteLine(cadena)
+
+
+        Console.WriteLine()
+        Console.WriteLine()
+        Console.WriteLine()
 
 
         Dim v() As String
@@ -23,24 +31,31 @@ Module Program
         Dim x As Integer
 
         For x = 0 To v.Length - 1
-            Console.WriteLine(v(x))
+            'Console.WriteLine(v(x))
             subcad = v(x)
             For I = 0 To subcad.Length - 1
+                letra = subcad.Substring(I, 1)
 
-                If letra >= "A" And letra <= "Z" Then
+                If letra = "el" Or letra = "la" Or letra = "los" Or letra = "las" Or letra = "un" Or letra = "una" Or letra = "unos" Or letra = "unas" Then
                     contador = contador + 1
                 End If
-                letra = subcad.Substring(I, 1)
-                Console.WriteLine(letra)
-
-
+                'Console.WriteLine(letra)
             Next
-            Console.WriteLine()
 
+            If contador >= 1 Then
 
+                Console.ForegroundColor = ConsoleColor.Red
+                Console.Write(v(x) + " ")
+                Console.ForegroundColor = ConsoleColor.White
+                contador = 0
+            Else
+                Console.Write(v(x) + " ")
+            End If
 
         Next
 
-        Console.WriteLine(contador)
+        Console.WriteLine()
+        Console.WriteLine()
+        Console.WriteLine()
     End Sub
 End Module
--- ejercicio5/Program.vb
+++ ejercicio5/Program.vb
@@ -9,6 +9,63 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Dim cadena, subcad, palabra, letra As String
+        Dim contador = 0
+        Dim largo = 0
+        cadena = "El Índice TIOBE es un índice elaborado por una empresa de software holandesa que se especializa en la evaluación y seguimiento de la calidad de los programas informáticos. Actualmente, la empresa revisa en tiempo real más de 300 millones de códigos de diversos programas informáticos por día actualizando mes a mes su índice."
+
+        Console.WriteLine(cadena)
+        Console.WriteLine()
+        Console.WriteLine()
+        Console.WriteLine()
+        For x = 0 To cadena.Length - 1
+            letra = cadena.Substring(x, 1)
+
+            If letra = "a" Then
+                letra = 1
+            End If
+            If letra = "e" Then
+                letra = 2
+            End If
+            If letra = "i" Then
+                letra = 3
+            End If
+            If letra = "o" Then
+                letra = 4
+            End If
+            If letra = "u" Then
+                letra = 5
+            End If
+
+
+
+
+
+
+
+            If letra = "A" Then
+                letra = 6
+            End If
+            If letra = "E" Then
+                letra = 7
+            End If
+            If letra = "I" Then
+                letra = 8
+            End If
+            If letra = "O" Then
+                letra = 9
+            End If
+            If letra = "U" Then
+                letra = "#"
+            End If
+
+
+            Console.Write(letra)
+        Next
+
+        Console.WriteLine()
+        Console.WriteLine()
+        Console.WriteLine()
+
     End Sub
 End Module
