dec841a830a3ac5f9cd4657fdbbe56546024bdff
carlosmartin667 <carloscornejomoscoso@gmail.com>
2019-10-30 09:33:22 -0300

    ejercicio 1 resuellto

--- ejercicio1/Program.vb
+++ ejercicio1/Program.vb
@@ -9,36 +9,45 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.BackgroundColor = ConsoleColor.White
-        Console.ForegroundColor = ConsoleColor.Red
+        'Console.BackgroundColor = ConsoleColor.White
+        'Console.ForegroundColor = ConsoleColor.Red
         Console.Clear()
-
-
         Dim cadena, subcad, palabra As String
+        Dim contador = 0
         cadena = "El Índice TIOBE es un índice elaborado por una empresa de software holandesa que se especializa en la evaluación y seguimiento de la calidad de los programas informáticos. Actualmente, la empresa revisa en tiempo real más de 300 millones de códigos de diversos programas informáticos por día actualizando mes a mes su índice."
         Console.WriteLine(cadena)
-
-
         Dim v() As String
         v = cadena.Split(" ")
-
-        palabra = Console.ReadLine()
-
         Console.WriteLine()
-        Console.WriteLine(palabra)
-
-
-
+        Console.WriteLine("INGRESE PALABRA QUE QUIERE BUSCAR ??")
+        Console.WriteLine()
+        Console.ForegroundColor = ConsoleColor.Green
+        palabra = Console.ReadLine()
+        Console.WriteLine()
         Dim x As Integer
-
         For x = 0 To v.Length - 1
-
             If palabra = v(x) Then
-                Console.WriteLine(v(x))
+                contador = contador + 1
             End If
-
         Next
+        Console.ForegroundColor = ConsoleColor.White
+        If contador >= 1 Then
+            For x = 0 To v.Length - 1
+                If palabra = v(x) Then
+                    Console.ForegroundColor = ConsoleColor.Red
+                    Console.Write(v(x) + " ")
+                    Console.ForegroundColor = ConsoleColor.White
+                Else
+                    Console.Write(v(x) + " ")
+                End If
+            Next
+        Else
+            Console.BackgroundColor = ConsoleColor.White
+            Console.Clear()
 
-
+            Console.ForegroundColor = ConsoleColor.Red
+            Console.Write("NO SE ENCONTRO LA PALABRA  ")
+            Console.Write("")
+        End If
     End Sub
 End Module
