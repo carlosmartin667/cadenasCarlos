866a93d64cce7470203c06f6f541ec596634c122
carlosmartin667 <carloscornejomoscoso@gmail.com>
2019-12-02 16:57:03 -0300

    v

--- EXAMEN/Program.vb
+++ EXAMEN/Program.vb
@@ -1,7 +1,64 @@
-Imports System
+Module Module1
+    'En base de un texto
+    'Mostrarlo en color verde
+    'sacarle los signos de puntuacion 
+    'Los numeros remplazarlo por #
+    'Buscar una palabra (texto en rojo, palara encontrada en blanco) si no la encuentra mensaje en azul de palabra no encontrada.
+    'comprobar cuantas palabras hay repetidas texto mostrar palabras repetidas y cuantas veces
+    Sub Main()
+        Dim texto As String
+        texto = "Quiere la boca exhausta vid, kiwi, piña y fugaz jamón. (42 letras) Fabio me exige, sin tapujos, que añada cerveza al whisky. (45 letras) Jovencillo emponzoñado de whisky, ¡qué figurota exhibe! (46 letras)"
+        Console.WriteLine(texto)
+        Console.ForegroundColor = ConsoleColor.Green
+        Console.WriteLine(texto)
+        texto = texto.Replace(",", "")
+        texto = texto.Replace(".", "")
+        texto = texto.Replace("?", "")
+        texto = texto.Replace("(", "")
+        texto = texto.Replace(")", "")
+        texto = texto.Replace("¿", "")
+        texto = texto.Replace("1", "#")
+        texto = texto.Replace("2", "#")
+        texto = texto.Replace("3", "#")
+        texto = texto.Replace("4", "#")
+        texto = texto.Replace("5", "#")
+        texto = texto.Replace("9", "#")
+        texto = texto.Replace("8", "#")
+        texto = texto.Replace("7", "#")
+        texto = texto.Replace("6", "#")
+        Console.WriteLine(texto)
+        Console.ForegroundColor = ConsoleColor.Gray
 
-Module Program
-    Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Dim Palabra, v() As String
+        Dim x As Integer
+        v = texto.Split(" ")
+        Do While True
+            Console.WriteLine()
+            Console.Write("Ingrese palabra: ")
+            Console.ForegroundColor = ConsoleColor.Red
+            Palabra = Console.ReadLine()
+
+            If Palabra = "" Then Exit Do
+
+            If texto.Contains(Palabra) = True Then
+                For x = 0 To v.Length - 1
+                    If v(x) = Palabra Then
+                        Console.ForegroundColor = ConsoleColor.White
+                        Console.Write(v(x) & " ")
+                        Console.ForegroundColor = ConsoleColor.Red
+                    Else
+                        Console.Write(v(x) & " ")
+                    End If
+                Next
+                Console.WriteLine()
+            Else
+                Console.WriteLine()
+                Console.ForegroundColor = ConsoleColor.Blue
+                Console.WriteLine("LA PALABRA NO SE ENCUENTRA")
+                Console.ForegroundColor = ConsoleColor.White
+            End If
+        Loop
+
     End Sub
+
 End Module
---
+++ n1/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace n1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto, palabra;
+            int contador = 0;
+            int palabrasRepetidasContador = 0;
+            List<string> a = new List<string>();
+            var duplicates = new List<string>();
+            var duplicates2 = new List<string>();
+            var duplicatesCaantidad = new List<int>();
+       
+            texto = "La tecnología 5G se muestra al mundo como uno de los pilares del futuro más próximo. Los smartphones de los años venideros tendrán como base la quinta generación de cobertura, y muchas son las marcas que ya han apostado por este tipo de soportes. Xiaomi, Samsung, Huawei, LG… Los fabricantes ya están trabajando en terminales con tecnología 5G integrada en sus procesadores, el único problema es que los países todavía no están preparados para recibir estas novedades.";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(texto);
+
+            string[] v = new string[texto.Length];
+            texto = texto.Replace(".", " ");
+            texto = texto.Replace(",", " ");
+            texto = texto.Replace(":", " ");
+            texto = texto.Replace(";", " ");
+            texto = texto.Replace("?", " ");
+
+            for (int i = 0; i < 10; i++)
+            {
+                texto = texto.Replace(Convert.ToString(i), "#");
+            }
+
+            v = texto.Split(" ");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(texto);
+            Console.WriteLine();
+
+            do
+            {
+                Console.WriteLine("INGRESE PALABRA QUE QUIERE BUSCAR ??");
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                palabra = Console.ReadLine();
+                Console.WriteLine();
+
+                if (palabra == "0")
+                {
+                    break;
+                }
+                for (int i = 0; i < v.Length; i++)
+                {
+                    if (palabra == v[i])
+                    {
+                        contador = contador + 1;
+
+                    }
+                }
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine();
+
+
+                if (contador > 0)
+                {
+                    for (int i = 0; i < v.Length; i++)
+                    {
+                        if (palabra == v[i])
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.Write(v[i] + " ");
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            contador = 0;
+                        }
+                        else
+                        {
+                            Console.Write(v[i] + " ");
+                            contador = 0;
+                        }
+                    }
+                }
+                else
+                {
+
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("NO SE ENCONTRO LA PALABRA  ");
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    contador = 0;
+                }
+            } while (true);
+
+
+            foreach (var s in v)
+            {
+                if (s != "")
+                {
+                    if (!a.Contains(s))
+                        a.Add(s);
+                    else
+                        duplicates.Add(s);
+                }
+
+            }
+            foreach (var item in duplicates)
+            {
+                if (!duplicates2.Contains(item))
+                    duplicates2.Add(item);
+            }
+
+
+
+            foreach (var s in duplicates2)
+            {
+
+                for (int i = 0; i < v.Length; i++)
+                {
+                    if (s == v[i])
+                    {
+                        contador = contador + 1;
+                    }
+                }
+
+                duplicatesCaantidad.Add(contador);
+                contador = 0;
+                palabrasRepetidasContador = palabrasRepetidasContador + 1;
+            }
+
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+
+            for (int i = 0; i < palabrasRepetidasContador; i++)
+            {
+                Console.Write("palabra  repetida :");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write($" {duplicates2[i] } ");
+
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.Write(", candidad de repeticiones : ");
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write($"{duplicatesCaantidad[i]} ");
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Magenta;
+            }
+
+
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+        }
+    }
+}
